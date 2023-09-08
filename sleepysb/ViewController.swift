587c749aa896bbb70af92c32034ffbd5b6c17c4a
Winlentia <winlentia@gmail.com>
2023-09-09 02:04:24 +0300

    Initial Commit

---
+++ sleepysb/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  sleepysb
+//
+//  Created by Winlentia on 9.09.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
