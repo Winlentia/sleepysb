3824f0dc96cb2987cc1f5910a16c0a9400b0a266
Winlentia <winlentia@gmail.com>
2023-09-24 00:31:31 +0300

    ui maked

---
+++ sleepysb/Managers/ShellManager.swift
@@ -0,0 +1,49 @@
+//
+//  ShellManager.swift
+//  sleepysb
+//
+//  Created by Winlentia on 23.09.2023.
+//
+
+import Foundation
+
+class ShellManager {
+    
+    func sleepMac(){
+        do {
+            try safeShell("pmset sleepnow")
+        } catch {
+            print("\(error)")
+        }
+    }
+    
+    @discardableResult // Add to suppress warnings when you don't want/need a result
+    func safeShell(_ command: String) throws -> String {
+        let task = Process()
+        let pipe = Pipe()
+        
+        task.standardOutput = pipe
+        task.standardError = pipe
+        task.arguments = ["-c", command]
+        task.executableURL = URL(fileURLWithPath: "/bin/bash")
+        task.standardInput = nil
+
+        try task.run() //<--updated
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8)!
+        
+        return output
+    }
+    
+    @discardableResult
+    func shell(_ args: String...) -> Int32 {
+        let task = Process()
+        task.launchPath = "/usr/bin/env"
+        task.arguments = args
+        task.launch()
+        task.executableURL = URL(fileURLWithPath: "/bin/bash")
+        task.waitUntilExit()
+        return task.terminationStatus
+    }
+}
--- sleepysb/ViewController.swift
+++ sleepysb/ViewController.swift
@@ -8,11 +8,21 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    let shellManager = ShellManager()
+    var isTimerActive: Bool = false
+    var timer: Timer? = nil
 
+    @IBOutlet weak var textFieldSeconds: NSTextField!
+    @IBOutlet weak var textFieldMinutes: NSTextField!
+    @IBOutlet weak var textFieldHour: NSTextField!
+    
+    @IBOutlet weak var buttonStart: NSButton!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTextFields()
     }
 
     override var representedObject: Any? {
@@ -20,7 +30,85 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    fileprivate func setupTextFields() {
+        textFieldSeconds.delegate = self
+        textFieldHour.delegate = self
+        textFieldMinutes.delegate = self
+        
+        let onlyIntFormatter = OnlyIntegerValueFormatter()
+        
+        textFieldHour.formatter = onlyIntFormatter
+        textFieldMinutes.formatter = onlyIntFormatter
+        textFieldSeconds.formatter = onlyIntFormatter
+        
+        textFieldSeconds.stringValue = "0"
+        textFieldMinutes.stringValue = "0"
+        textFieldHour.stringValue = "0"
+    }
 
 
+    @IBAction func actionStart(_ sender: Any) {
+        isTimerActive = !isTimerActive
+        
+        if isTimerActive {
+            buttonStart.title = "Stop"
+            print(calculateTimerInSeconds())
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+        } else {
+            buttonStart.title = "Start"
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+    
+    @objc func fireTimer() {
+        print("Timer fired!")
+    }
+    
+    func calculateTimerInSeconds() -> Int {
+//        return (textFieldSeconds.intValue) + (textFieldMinutes.intValue * 60) + (textFieldHour.intValue * 60 * 60)
+//        return Int(textFieldSeconds.stringValue) + Int(text)
+        let seconds = Int(textFieldSeconds.stringValue) ?? 0
+        let minutes = (Int(textFieldMinutes.stringValue) ?? 0) * 60
+        let hours = (Int(textFieldHour.stringValue) ?? 0) * 60 * 60
+        return seconds + minutes + hours
+    }
+    
+    let deadlineMinutes = DispatchTime.now() + 60*1.0
 }
 
+
+extension ViewController: NSTextFieldDelegate {
+    func controlTextDidChange(_ obj: Notification) {
+        let textField = obj.object as? NSTextField
+        
+        if textField === textFieldHour {
+            print("textFieldHour \(textFieldHour.stringValue)")
+        } else if textField === textFieldMinutes {
+            print("textFieldHour \(textFieldMinutes.stringValue)")
+        } else if textField === textFieldSeconds {
+            print("textFieldHour \(textFieldSeconds.stringValue)")
+        }
+    }
+}
+
+class OnlyIntegerValueFormatter: NumberFormatter {
+    
+    override func isPartialStringValid(_ partialString: String, newEditingString newString: AutoreleasingUnsafeMutablePointer<NSString?>?, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
+        
+        // Ability to reset your field (otherwise you can't delete the content)
+        // You can check if the field is empty later
+        if partialString.isEmpty {
+            return true
+        }
+        
+        // Optional: limit input length
+        if partialString.count > 2 {
+            return false
+        }
+        
+        // Actual check
+        return Int(partialString) != nil
+    }
+}
