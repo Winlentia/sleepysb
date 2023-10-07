c6dcaf152e1470d3083813579321ad8d7eb8d7c8
Winlentia <winlentia@gmail.com>
2023-10-08 01:41:24 +0300

    AppStatusBar is now enumurated

--- sleepysb/AppDelegate.swift
+++ sleepysb/AppDelegate.swift
@@ -24,13 +24,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-    
+}
+
+
+//MARK: MenuItem
+
+extension AppDelegate {
     @objc func menuItemSelector(_ sender: Any?) {
-        guard let menuItem = sender as? NSMenuItem else { return }
-        
-        if menuItem.title == "Print Quote" {
-            print("Print Quote")
+        guard let menuItem = sender as? AppMenuItem else { return }
+        switch menuItem.appStatusBarItem {
+        case .sleep15Min:
+            print(menuItem.title)
+        case .sleep30Min:
+            print(menuItem.title)
+        case .sleep45Min:
+            print(menuItem.title)
+        case .sleep1Hour:
+            print(menuItem.title)
         }
+        
     }
 }
 
--- sleepysb/AppStatusBar.swift
+++ sleepysb/AppStatusBar.swift
@@ -18,10 +18,61 @@ class AppStatusBar {
 
         let menu = NSMenu()
 
-        menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.menuItemSelector(_:)), keyEquivalent: "P"))
+        for item in AppStatusBarMenuItem.allCases {
+            menu.addItem(AppMenuItem(appStatusBarItem: item))
+        }
         menu.addItem(NSMenuItem.separator())
         menu.addItem(NSMenuItem(title: "Quit Quotes", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
         menuBarItem.menu = menu
+    }
+}
+
+class AppMenuItem: NSMenuItem {
+    
+    let appStatusBarItem: AppStatusBarMenuItem
+    
+    init(appStatusBarItem: AppStatusBarMenuItem){
+        self.appStatusBarItem = appStatusBarItem
+        super.init(title: appStatusBarItem.title(), action: #selector(AppDelegate.menuItemSelector(_:)), keyEquivalent: appStatusBarItem.keyEquivalent())
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+enum AppStatusBarMenuItem: CaseIterable {
+    case sleep15Min
+    case sleep30Min
+    case sleep45Min
+    case sleep1Hour
+}
+
+extension AppStatusBarMenuItem {
+    func title() -> String {
+        switch self {
+        case .sleep15Min:
+            return "Sleep 15 Min"
+        case .sleep30Min:
+            return "Sleep 30 Min"
+        case .sleep45Min:
+            return "Sleep 45 Min"
+        case .sleep1Hour:
+            return "Sleep 1 Hour"
+        }
+    }
+    
+    func keyEquivalent() -> String {
+        switch self {
+        case .sleep15Min:
+            return "1"
+        case .sleep30Min:
+            return "2"
+        case .sleep45Min:
+            return "3"
+        case.sleep1Hour:
+            return "4"
+        }
     }
 }
 
